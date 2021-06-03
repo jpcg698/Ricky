aac0ebd540d3b11936b84e320475f662cc23ab66
Luis Ricardo Espinoza Rodriguez <ricardo.espinoza@ricardo.espinoza>
2021-06-03 17:45:10 -0500

    juanpis

--- fetch.js
+++ fetch.js
@@ -1,22 +1,25 @@
 const fetch = require('node-fetch')
 
 const getPokemons = async ({ limit = 10 }) => {
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=${limit}');
-    const nose = await res.json();
+    //Use fetch to this API https://pokeapi.co/api/v2
+    //Make sure to not retrieve one specific pokemon, you need to retrieve multiple pokemons
+    const res =  siguele aqui jeipi
+    const jsonParseado = eee investigale papi
     return nose;
 }
 
 const getPokemon = async (limit) => {
     let poke = await getPokemons({ limit });
-    const pokemons = poke.results.map(async ({ url }) => {
-        const res = await (await fetch(url)).json();
-        const { weight, name } = res;
+    const pokemons = poke.results.map(({ url }) => {
+        //Fetch one specific pokemon for every url in the poke.results array
+        //Then get the weight and name from the result and return it
         return { weight, name };
     });
     return pokemons
 }
 
-getPokemon(2).then(lmao => {
-    Promise.all(lmao).then(owo => console.log(owo))
+getPokemon(2).then(pokemon => {
+    //console.log every pokemon
+    //This is the expected output
 })
 
