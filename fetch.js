589bc0a3cf7b594679b67f2da040162f92f55891
Luis Ricardo Espinoza Rodriguez <ricardo.espinoza@ricardo.espinoza>
2021-06-03 17:46:18 -0500

    lmao

--- fetch.js
+++ fetch.js
@@ -21,5 +21,29 @@ const getPokemon = async (limit) => {
 getPokemon(2).then(pokemon => {
     //console.log every pokemon
     //This is the expected output
+    /*
+    [
+        { weight: 69, name: 'bulbasaur' },
+        { weight: 130, name: 'ivysaur' },
+        { weight: 1000, name: 'venusaur' },
+        { weight: 85, name: 'charmander' },
+        { weight: 190, name: 'charmeleon' },
+        { weight: 905, name: 'charizard' },
+        { weight: 90, name: 'squirtle' },
+        { weight: 225, name: 'wartortle' },
+        { weight: 855, name: 'blastoise' },
+        { weight: 29, name: 'caterpie' },
+        { weight: 99, name: 'metapod' },
+        { weight: 320, name: 'butterfree' },
+        { weight: 32, name: 'weedle' },
+        { weight: 100, name: 'kakuna' },
+        { weight: 295, name: 'beedrill' },
+        { weight: 18, name: 'pidgey' },
+        { weight: 300, name: 'pidgeotto' },
+        { weight: 395, name: 'pidgeot' },
+        { weight: 35, name: 'rattata' },
+        { weight: 185, name: 'raticate' }
+      ]
+    */
 })
 
